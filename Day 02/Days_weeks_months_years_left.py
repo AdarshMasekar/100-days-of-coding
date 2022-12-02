58ecb80989d49daba0f67d18f1ba458dc01bc133
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-12-02 20:07:00 +0530

    02Dec22

--- Day 02/Days_weeks_months_years_left.py
+++ Day 02/Days_weeks_months_years_left.py
@@ -11,5 +11,5 @@ monthsleft = yearsLeft * 12
 weeksLeft = yearsLeft * 52
 
 daysLeft = monthsleft * 30
-
+ 
 print(f"You have {daysLeft} days, {weeksLeft} weeks, {monthsleft} months and {yearsLeft} years")
