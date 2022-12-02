590bf074caa6ed29fa8afc57b00fab994cd781b4
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-12-02 19:08:44 +0530

    02Dec2022

---
+++ Day 01/Band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand name Generator.")
+city = input("What's name of  the ciyt you grew up in ?\n")
+pet = input("What's your pet's name?\n")
+print( "Your brand name could be "+city+" " +pet)
