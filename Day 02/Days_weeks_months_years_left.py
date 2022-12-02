8bb1e55564c5a0f8cc809c056cd00932bf1a7903
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-12-02 20:06:41 +0530

    Create Days_weeks_months_years_left.py

---
+++ Day 02/Days_weeks_months_years_left.py
@@ -0,0 +1,15 @@
+"""
+if a person is able to live 90 years print  the remaining days,weeks,months and years he/she has from their current age
+"""
+
+age = int(input("Enter your age : "))
+
+yearsLeft = 90 - age 
+
+monthsleft = yearsLeft * 12
+
+weeksLeft = yearsLeft * 52
+
+daysLeft = monthsleft * 30
+
+print(f"You have {daysLeft} days, {weeksLeft} weeks, {monthsleft} months and {yearsLeft} years")
