b9e47cc091382a2104a2a453268fd97bfbd830a5
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-12-16 23:49:15 +0530

    Update loops.py

--- Day 13/loops.py
+++ Day 13/loops.py
@@ -1,3 +1,8 @@
 for i in range(0,20):
   if i == 20:         # i will never reach 20 come inside the loop so below statements are useless
     print("end")
+    
+    
+
+while true:
+  print ("hello")
